5261bb01355f900a4b3b06ae16965c06893da662
kentbogestam <kent@cumbari.com>
2016-07-11 21:08:46 +0200

    fixed moblyo link

--- Links.h
+++ Links.h
@@ -19,7 +19,7 @@
 
 #define UseCouponURL @"https://market.cumbari.com/CouponServer/clientapi/useCoupon?apiVersion=1&token=cumba4ever&couponId="
 
-#define CumbariURL @"https://http://moblyo.de/mobiles-lokales-marketing/"
+#define CumbariURL @"http://moblyo.de/impressum"
 
 #define GetHostURL @"https://market.cumbari.com/CouponServer/clientapi/getHostURL?longitude=1&latitude=1&token=cumba4ever"
 
